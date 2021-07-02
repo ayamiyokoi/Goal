9913fef9e873bb19e1b50e0fbc588e84bcd29d9c
AyamiYokoi <didufd63hff@gmail.com>
2021-07-02 02:48:03 +0000

    [Add]ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'follows/index'
-  resources :tasks
-  get 'goals/index'
-  get 'goals/edit'
-  get 'notifications/index'
-  resources :events
-  get 'tags/index'
-  get 'comments/index'
-  resources :reviews
-  get 'chats/index'
-  resources :groups
+  root to: 'homes#top'
+  get '/about' => 'homes#about'
+  resources :follows, only: [:index, :create, :destroy]
+  resources :events, :reviews, :groups
+  resources:tasks, except: [:new]
+  resources :goals, except: [:new, :show]
+  get 'notifications/destroy_all' => 'notifications#destroy_all'
+  resources :notifications, only: [:index]
+  resources :likes, only: [:create, :destroy]
+  resources :tags, only: [:index, :create, :update, :destroy]
+  resources :comments, only: [:index, :create, :update, :destroy]
+  resources :chats, only: [:index, :create, :update, :destroy]
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
