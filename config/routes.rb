f747c245d62061264d1e111f7224bd46c57982c3
AyamiYokoi <didufd63hff@gmail.com>
2021-07-02 05:34:03 +0000

    [Add]header

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/about' => 'homes#about'
   resources :follows, only: [:index, :create, :destroy]
   resources :events, :reviews, :groups
-  resources:tasks, except: [:new]
+  resources :tasks
   resources :goals, except: [:new, :show]
   get 'notifications/destroy_all' => 'notifications#destroy_all'
   resources :notifications, only: [:index]
@@ -13,6 +13,6 @@ Rails.application.routes.draw do
   resources :tags, only: [:index, :create, :update, :destroy]
   resources :comments, only: [:index, :create, :update, :destroy]
   resources :chats, only: [:index, :create, :update, :destroy]
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
