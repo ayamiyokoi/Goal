3bc516248ac17080923dc36f618db98c6eb48381
AyamiYokoi <didufd63hff@gmail.com>
2021-07-02 08:02:01 +0000

    [Add]chat機能コントローラ

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,4 +1,16 @@
 class ChatsController < ApplicationController
   def index
+    @chats = Chat.all
+    @chat = Chat.new
   end
+  
+  def create
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
   attachment :profile_image
   has_many :group_users
   has_many :groups, through: :group_users
+  has_many :chats, dependent: :destroy
 end
