c30eb78e417439cb39d88e0398885358d7e27f99
AyamiYokoi <didufd63hff@gmail.com>
2021-07-07 12:42:49 +0000

    [Fix]引数調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @review = Review.where(user_id: @user.id).limit(1)
+    @review = Review.where(user_id: @user.id).first
   end
 
   def index
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,19 +7,7 @@ class Comment < ApplicationRecord
   # コメント通知機能
   def create_notification_comment(current_user, review_id, commented_id, comment_id)
     # 自分の投稿に対して自分以外にコメントしている人をすべて取得し、通知を送る
-
-
-    # reviews_ids.each do |reviews_id|
-    #   temp_ids = Comment.select(:user_id).where(review_id: reviews_id).where.not(user_id: current_user.id).distinct
-    #   # binding.irb
-    #   temp_ids.each do |temp_id|
      save_notification_comment(current_user, review_id, commented_id, comment_id)
-        # save_notification_comment(current_user, comment_id, temp_id['user_id'], review_id)
-    #   end
-    # end
-    # # まだ誰もコメントしていない場合は、投稿者に通知を送る
-    # save_notification_comment(current_user, comment_id, user_id, review_id) if temp_ids.blank?
-
   end
 
    def save_notification_comment(current_user, review_id, visited_id, comment_id)
