48eb8999e4e7d024a1cf7a4b0baf7a6a8f1b2235
AyamiYokoi <didufd63hff@gmail.com>
2021-07-12 12:53:49 +0000

    [Add]週間グラフ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,12 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @review = Review.where(user_id: @user.id).first
-    # from  = Time.current.at_beginning_of_day
-    # to    = (from - 6.day).at_end_of_day
-    @data = Review.where(user_id: @user.id).pluck(:created_at, :rate)
-    @to = Time.current.at_beginning_of_day
-    @from = (@to - 6.day).at_end_of_day
+    @to = Time.current.at_end_of_day + (7 * params[:week_id].to_i).day
+    @from = (@to - 6.day).at_beginning_of_day
+
+    @data = Review.where(user_id: @user.id).where(created_at: @from..@to).pluck(:created_at, :rate).map do |d|
+      [d.first.beginning_of_day, d.second]
+    end
     unless @review == nil
       @rate = {"達成" => @review.rate, "未達成" => 100-@review.rate}
     end
--- config/initializers/chartkick.rb
+++ config/initializers/chartkick.rb
@@ -2,12 +2,10 @@
 
 Chartkick.options = {
   xtitle: "日付",
-  ytitle: "達成率",
+  ytitle: "達成率 (%)",
   width: "800px",
   height: "500px",
   min: 0,
   max: 100,
-  xmin: @from,
-  xmax: @to,
   empty: "データがありません"
 }
