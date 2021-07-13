024457c35c9d3862ccee6de5b2d3e6f92b1c986a
AyamiYokoi <didufd63hff@gmail.com>
2021-07-13 10:00:20 +0000

    [Edit]reviewをあれば更新、なければ新規登録に変更

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -15,9 +15,13 @@ class GoalsController < ApplicationController
     @goal = Goal.new(goal_params)
     @goal.user_id = current_user.id
     if @goal.save
+      #目標設定の個数が２のべき乗ごとにステージが上がる設定
+      #自分の目標の数が２の自分のステージ乗であるなら
       if Goal.where(user_id: current_user.id).count == 2**current_user.stage
+        #今のステージから1上がる
         current_user.stage = current_user.stage + 1
-        current_user.update
+        byebug
+        current_user.update(user_params)
       end
       redirect_to request.referer
     else
@@ -49,5 +53,9 @@ class GoalsController < ApplicationController
 
   def goal_params
     params.require(:goal).permit(:name, :date, :achieved)
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction, :level)
   end
 end
