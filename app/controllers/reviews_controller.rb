fe957e9e0b0b2be6fc9cd2a3c7ed28a3f55fe4ff
AyamiYokoi <didufd63hff@gmail.com>
2021-07-22 03:25:57 +0000

    [Fix]言い値のデータのとり方

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,7 +4,6 @@ class ReviewsController < ApplicationController
   before_action :set_review_all, only: %i[ index topics ]
   before_action :set_review_mine, only: %i[ index topics ]
   before_action :set_review_liked, only: %i[ topics ]
-  #TODO: なんかエラー出る、直して
   before_action :set_review_know, only: %i[ index topics ]
   before_action :set_review, only: %i[ show edit update destroy ]
 
@@ -13,9 +12,10 @@ class ReviewsController < ApplicationController
   end
 
   def topics
+     #TODO: どこにincludeつける?
     #TODO: 自分の友達のlike, status 2のlikeだけ取る
     # @reviews_like = Kaminari.paginate_array(Review.sorted_by_likes).page(params[:page]).per(10)
-    @reviews_like = Kaminari.paginate_array(Review.joins(:user).where(user_id: {show_status: 2}).sorted_by_likes).page(params[:page]).per(10)
+    @reviews_like = Kaminari.paginate_array(Review.where(user_id: current_user.friends.pluck(:id)).or(Review.where(user_id: User.where(show_status: 2).pluck(:id))).sorted_by_likes).page(params[:page]).per(10)
     #@reviews_like = Kaminari.paginate_array(Review.joins(:user).select("reviews.*, user.*").where(show_status: 2).sorted_by_likes).page(params[:page]).per(10)
     # @reviews_like = Review.sorted_by_likes
   end
@@ -90,9 +90,9 @@ class ReviewsController < ApplicationController
   private
 
     def set_review_all
-      #TODO: きれいにして
-      # @review_all = Review.where(user_id: User.where(show_status: 2).pluck('id')).page(params[:page]).per(10)
-      @reviews_all = User.where(show_status: 2).page(params[:page]).per(10)
+      #TODO: 右取れてるのになんで表示されん？
+      @review_all = Review.where(user_id: User.where(show_status: 2).pluck(:id)).page(params[:page]).per(10)
+    #   @reviews_all = User.where(show_status: 2).page(params[:page]).per(10)
     end
 
     def set_review_mine
@@ -105,7 +105,8 @@ class ReviewsController < ApplicationController
 
     def set_review_know
       # @reviews_know = current_user.friends.page(params[:page]).per(10)
-      @reviews_know = Review.where(user_id: current_user.friends.pluck(:id)).page(params[:page]).per(10)
+      #N+1問題対策
+      @reviews_know = Review.where(user_id: current_user.friends.pluck(:id)).includes([:user]).page(params[:page]).per(10)
     end
     # Use callbacks to share common setup or constraints between actions.
     def set_review
