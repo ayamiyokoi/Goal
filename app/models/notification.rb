23caf616ac5cbf3e56aa20799250415dbd3fa385
AyamiYokoi <didufd63hff@gmail.com>
2021-07-28 10:03:55 +0000

    [Edit]マイページのレイアウト変更

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,6 +3,8 @@
 class Notification < ApplicationRecord
   belongs_to :review, optional: true
   belongs_to :comment, optional: true
+  belongs_to :chat, optional: true
+  belongs_to :group, optional: true
 
   belongs_to :visitor, class_name: "User", foreign_key: "visitor_id", optional: true
   belongs_to :visited, class_name: "User", foreign_key: "visited_id", optional: true
