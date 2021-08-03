8009104da34e5305737b0dde9cb09fa7e06c8c5c
AyamiYokoi <didufd63hff@gmail.com>
2021-08-03 04:07:49 +0000

    [Add]カレンダーバリデーション

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,4 +6,12 @@ class Event < ApplicationRecord
   validates :title, :presence => { :message => "を入力してください" }
   validates :start_time, :presence => { :message => "を入力してください" }
   validates :end_time, :presence => { :message => "を入力してください" }
+  validate :start_end_check
+
+  def start_end_check
+    unless self.end_time == nil
+      errors.add(:end_time, "は開始日時より前の日付は登録できません。") unless
+      self.start_time < self.end_time
+    end
+  end
 end
